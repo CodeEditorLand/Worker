bf841e932d970fe47163608b897836b444a8813d
Nikola Hristov <Nikola@PlayForm.Cloud>
2025-06-18 07:33:40 +0300

    sync
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/prepublishOnly.sh
+++ Source/prepublishOnly.sh
@@ -3,5 +3,5 @@
 Build "Source/Configuration/**/*.ts" \
 	--ESBuild Source/Configuration/ESBuild/Worker.ts
 
-Build Build 'Source/**/*.ts' \
+Build "Source/**/*.ts" \
 	--ESBuild Configuration/ESBuild/Target.js
